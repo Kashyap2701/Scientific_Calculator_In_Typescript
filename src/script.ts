238e28dfed9f6ae51cd8be19350dd90401d77c98
Kashyap Patel <kashyap.p@simformsolutions.com>
2023-04-13 11:31:18 +0530

    Add Event Listnerer

--- src/script.ts
+++ src/script.ts
@@ -408,3 +408,348 @@ for (let i = number; i >= 1; i--) {
 }
 return fact;
 }
+
+// ------------------------- Event Listnerer ---------------------------------
+// fixed decimal and exponential format
+FEButton.addEventListener("click",()=>{
+
+    if(curInput.value){
+      curInput.value = Number(curInput.value).toExponential(2);
+      c.actualExpression += Number(curInput.value) * Math.pow(10,2);
+      display.value.includes("=") ? display.value = `(${curInput.value })` : display.value += `(${curInput.value })`;
+      c.isCalculatorInputAdd = true;
+    }
+  
+})
+  
+degToRed.addEventListener('click',(e)=>{
+    
+      let AngleText = degToRed.innerText;
+  
+      if(AngleText === "DEG"){
+          degToRed.innerText = "RAD";
+          c.angleInDegree = false;
+      }
+      else{
+          degToRed.innerText = "DEG";
+          c.angleInDegree=true; 
+      }
+})
+  
+digits.addEventListener('click',(e)=>{
+
+    let digitButton = e.target as HTMLInputElement;
+
+    if(digitButton.tagName === "BUTTON"){
+
+    if (digitButton.value === "-" ) {
+
+        let exp = Calculator.splitsByOperator(curInput.value).slice(0,-1).join("");
+        let number = Calculator.splitsByOperator(curInput.value).slice(-1).join("");
+
+        if(!isNaN(Number(curInput.value)))
+            curInput.value = (Number(number)*-1).toString();
+        else{
+            if(exp.charAt(exp.length-1)=="-"){
+            curInput.value = `${exp.slice(0,-1)}+${number}`;
+            }
+            else if(exp.charAt(exp.length-1)=="+"){
+            curInput.value = `${exp.slice(0,-1)}${Number(number)*-1}`;
+            }
+            else if(number == ")" && exp.charAt(0)=="("){
+            curInput.value = `-${exp}${number}`;
+            }
+            else if(number == ")" && exp.charAt(0)=="-"){
+            curInput.value = `${exp.slice(1)}${number}`;
+            }
+            else if(!isNaN(Number(number))){
+            curInput.value = `${exp}${Number(number)*-1}`;
+            }
+            else{
+            c.showError('Check your expression')
+            }
+        }
+    }
+    else if(digitButton.value === "."){
+        curInput.value += 
+        curInput.value.includes(".") && Calculator.splitsByOperator(curInput.value).slice(-1).join("").includes(".")
+            ? ''
+            : digitButton.value
+    }
+    else {
+        let lastVal = InputDisplay.getLastValue();
+        
+        if(lastVal==")" || lastVal==PI)
+        curInput.value += `*${digitButton.value}`;
+        else
+        curInput.value += digitButton.value;  
+    }
+    }
+})
+  
+operators.addEventListener('click',(e)=>{
+
+    const  operatorValue = (e.target as HTMLButtonElement).value;
+
+    if(curInput.value){
+
+    if((curInput.value.includes('(') || InputDisplay.getLastValue()==PI) && (InputDisplay.getLastValue()!=')')) {
+        curInput.value+=operatorValue;
+    }
+    
+    // equal to button is not pressed
+    else if (curInput.value && operatorValue!=="=") {
+    
+        console.log('cdzsdcs kc');
+        
+
+        if (display.value.includes("=")) {
+        c.actualExpression = display.value = curInput.value + operatorValue;
+        curInput.value = "";
+        } 
+
+        else {
+        c.actualExpression +=
+            c.isCalculatorInputAdd === false
+            ? curInput.value + operatorValue
+            : operatorValue;
+        display.value +=
+            c.isCalculatorInputAdd === false
+            ? curInput.value + operatorValue
+            : operatorValue;
+        curInput.value = "";
+        }
+        c.isCalculatorInputAdd = false;
+    }
+
+    }
+})
+  
+equalButton.addEventListener('click',(e)=>{
+
+if(curInput.value && !display.value.includes("=")){
+
+    if (c.isCalculatorInputAdd==true) {
+    console.log(display.value);
+    } 
+    else {
+    display.value += curInput.value;
+    c.actualExpression += curInput.value;
+    console.log(display.value);
+    }
+
+    if(isBalanced(c.actualExpression)){  
+    curInput.value = c.calculate().toString();
+    
+    if(isNaN(Number(curInput.value)) || curInput.value==undefined){
+        c.showError('Expression is not valid. Check it once!');
+        display.value = "";
+        curInput.value = c.actualExpression;
+    }
+    c.actualExpression = "";
+    c.isCalculatorInputAdd=false;
+    console.log(display.value);
+    }
+    else{
+    c.showError('Expression is not valid. Check it once!');
+    display.value = "";
+    c.actualExpression = "";
+    console.log(display.value);
+    } 
+}
+
+c.isEvaluated = true;
+
+})
+  
+memoryButtons.addEventListener('click',(e)=>{
+
+const button = e.target as HTMLInputElement;
+const inputValue = parseFloat(curInput.value);
+
+    switch (button.value){
+    case "MS":
+        if(inputValue && !isNaN(inputValue)){
+        Memory.storeNumber(Number(curInput.value));
+        button.setAttribute('class','button-disable');
+        button.parentNode!.firstElementChild!.removeAttribute('class');
+        button.parentNode!.firstElementChild!.nextElementSibling!.removeAttribute('class');
+        }
+        else
+        c.showError('Invalid Input');
+        break;
+    case "MC" :
+        Memory.clearNumber();
+        button.setAttribute('class','button-disable');
+        button.parentNode!.lastElementChild!.removeAttribute('class');
+        button.nextElementSibling!.setAttribute('class','button-disable');
+        break;
+    case "M+" :
+        if(inputValue && !isNaN(inputValue)){
+        Memory.addNumber(inputValue);
+        c.clearExpression();
+        }
+        else
+        c.showError('Input is Invalid');
+        break;
+    case "M-" :
+        if(inputValue && !isNaN(inputValue)){
+        Memory.subtractNumber(inputValue);
+        c.clearExpression();
+        }
+        else
+        c.showError('Input is Invalid');
+    break;
+    case "MR":
+        console.log(c.actualExpression);
+        curInput.value = Memory.getNumber().toString();
+        console.log(Memory.getNumber());
+    }
+
+
+})
+  
+calculatorButtons.addEventListener("click", (e) => {
+  
+    let calButton = e.target as HTMLInputElement;
+  
+    if(calButton.tagName === "BUTTON"){
+  
+      let inputValue:string|number = parseFloat(curInput.value);
+  
+      if (curInput.value !== '') {
+  
+        display.value = display.value.includes("=") ? "" : display.value;
+  
+        switch (calButton.value) {
+  
+          case "x^2":
+            display.value += `(${curInput.value}^2)`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = Math.pow(inputValue, 2).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "sqrt":
+            if (display.value.includes("=")) {
+              display.value = "";
+            }
+            display.value += `sqrt(${curInput.value})`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = Math.sqrt(inputValue).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "10^":
+            display.value += `(10^${curInput.value})`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = Math.pow(10, inputValue).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "exp":
+            display.value += `(10^${curInput.value})`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = Math.pow(10, inputValue).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "log(":
+            display.value += `log(${curInput.value})`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = Math.log10(inputValue).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "ln(":
+            display.value += `ln(${curInput.value})`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = Math.log(inputValue).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "x^y":
+            if (display.value.includes("=")) {
+              display.value = "";
+            }
+            display.value += `${curInput.value}^`;
+            c.actualExpression += curInput.value + "^";
+            curInput.value = "";
+            break;
+          case "!":
+            display.value += `fact(${curInput.value})`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = fact(inputValue).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "1/":
+            display.value += `(1/${curInput.value})`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = (1 / inputValue).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "|":
+            display.value += `|${curInput.value}|`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = curInput.value[0] == "-" ? curInput.value.slice(1) : curInput.value;
+            c.actualExpression += curInput.value;
+            break;
+          case "^3":
+           
+            display.value += `(${curInput.value}^3)`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = Math.pow(inputValue, 3).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "cbrt":
+           
+            display.value += `cbrt(${curInput.value})`;
+            c.isCalculatorInputAdd = true;
+            inputValue = Math.cbrt(inputValue).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "yrootx":
+            
+            curInput.value = `${curInput.value}^(1/`;
+            c.isCalculatorInputAdd = false;
+            break;
+          case "e^x":
+           
+            display.value += `(e^${curInput.value})`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = Math.pow(Math.E, inputValue).toString();
+            c.actualExpression += curInput.value;
+            break;
+          case "2^":
+            display.value += `(2^${curInput.value})`;
+            c.isCalculatorInputAdd = true;
+            inputValue = Math.pow(2, inputValue);
+            c.actualExpression += curInput.value;
+            break;
+          case "log(x,2)":
+            display.value += `log(${curInput.value},2)`;
+            c.isCalculatorInputAdd = true;
+            curInput.value = Math.log2(inputValue).toString();
+            c.actualExpression += curInput.value;
+            break;
+        }
+      }
+      switch (calButton.value) {
+        case "e":
+          let expConst = 2.718281828459045.toFixed(3);
+          curInput.value && (!isNaN(Number(InputDisplay.getLastValue()))) || InputDisplay.getLastValue()==PI || InputDisplay.getLastValue()=="e"
+             ? curInput.value += `*${expConst}`
+             : curInput.value += expConst;
+          break;
+        case "pi":
+          let pi = calButton.getAttribute('data-pi');
+          curInput.value && (!isNaN(Number(InputDisplay.getLastValue())) || InputDisplay.getLastValue()==PI || InputDisplay.getLastValue()=="e")
+             ? curInput.value += `*${PI}`
+             : curInput.value += PI;
+          break;
+        case "(":
+          curInput.value && ((!isNaN(Number(InputDisplay.getLastValue()))) || InputDisplay.getLastValue()==")")
+            ? curInput.value += `*${calButton.value}`
+            : curInput.value += calButton.value;
+          break;
+        case ")":
+          if(curInput.value.includes('('))
+            curInput.value += calButton.value;
+          break;
+      }
+    }
+});
